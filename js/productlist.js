03b6d4055422031539c4cc7aab251fc99a7e1e9e
fuggfuggfugg <pwskhisty1234>
2016-06-01 23:32:37 -0400

    first pass

---
+++ js/productlist.js
@@ -0,0 +1,598 @@
+var prodlist = {
+"beer": [{ 
+        "name":"Asahi" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"KingFisher" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Heineke" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"KingFisher" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Heineke" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"KingFisher" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Heineke" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"KingFisher" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Heineke" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Beer5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    }],
+"rum":[{ 
+        "name":"Rum1" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum4" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum5" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum4" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum5" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum4" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum5" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum4" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Rum5" , 
+        "badge":"",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    }],
+"scotch": [{ 
+        "name":"Scotch1" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Scotch5" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    }],
+"spirits": [{ 
+        "name":"Spirit1" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit3" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Spirit5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    }],
+"whiskey": [{ 
+        "name":"Whiskey1" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey3" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey5" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey3" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey5" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey3" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey5" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey3" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey5" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey2" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey3" , 
+        "badge":" instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey4" , 
+        "badge":"pricedrop ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Whiskey5" , 
+        "badge":" ",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    }],
+"wine": [{ 
+        "name":"Wine1" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine2" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine3" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine4" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    },{ 
+        "name":"Wine5" , 
+        "badge":"pricedrop instock",
+        "image": "img/prod-empty.png",
+        "size": "200ml" 
+    }]
+}
